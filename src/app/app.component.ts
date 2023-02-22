84704d27a31426684438c14f8af0fadbf6fd062a
AntoineLudwigGalli <antoineludwiggalli@gmail.com>
2023-02-22 12:29:49 +0100

    ajout totop button et mise en page respage d'accueil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'alice-le-site';
 
+  windowScrolled = false;
+
+
+  ngOnInit() {
+    window.addEventListener('scroll', () => {
+      this.windowScrolled = window.pageYOffset !== 0;
+    });
+  }
+
+  scrollToTop(): void {
+    window.scrollTo(0, 0);
+  }
+
 }
